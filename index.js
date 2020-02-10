284f220888673e9843531bc28a64bd855737f31b
Abhijeet Vadera <abhijeet.vadera121212@gmail.com>
2020-02-10 17:47:54 +0530

    all views as ejs set.

--- index.js
+++ index.js
@@ -2,6 +2,34 @@ const express = require('express');
 const app = express();
 const port = 7373;
 
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('pages/index', {page : 'home'});
+});
+
+app.get('/about-us', (req, res) => {
+    res.render('pages/about-us', {page : 'about'});
+});
+
+app.get('/services', (req, res) => {
+    res.render('pages/services', {page : 'services'});
+});
+
+app.get('/contact', (req, res) => {
+    res.render('pages/contact', {page : 'contact'});
+});
+
+app.get('/login', (req, res) => {
+    res.render('pages/blog', {page : 'login'});
+});
+
+app.get('/logout', (req, res) => {
+    res.redirect('/');
+});
+
 app.listen(port, (req, res) =>{
     console.log('App listning at Port : ' + port);
     
